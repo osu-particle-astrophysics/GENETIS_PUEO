d9f7b0795ce2c13e13409e3579e57edb7b19ecd6
Dylan Wells <dylanwells37@gmail.com>
2022-11-07 16:42:30 -0500

    Fix Bugs, Working Version

--- BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/fitnessFunction_PUEO.py
+++ BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/fitnessFunction_PUEO.py
@@ -8,6 +8,7 @@
 ## Imports
 import argparse
 import csv
+import re
 ## Arguments
 #scaleFactor and GeoScaleFactor are used in the AraSim Loop, not currently utilized for PUEO.
 parser = argparse.ArgumentParser()
@@ -16,17 +17,10 @@ parser.add_argument("NSEEDS", help="number of seeds", type=int)
 #parser.add_argument("scaleFactor", help=" scaling variable for the exponent in the constraint", type=float)
 parser.add_argument("antennaFile", help="path to antenna file", type=str)
 #parser.add_argument("GeoScaleFactor", help="Factor by which we scale dwon the antenna dimensions", type=float)
-
+g=parser.parse_args()
 fitnessScores = []
 #fitnessScores = [NPOP, 0.0]
-def main(): #Loops through the antennas,calculates their fitness, and writes them to a csv
-	for i in range(g.NPOP):
-		fitness = ReadVeff(g.NSEEDS, g.antennaFile+"_"+str(i)+".txt")
-		fitnessScores.append(fitness)
-	WriteFitness()
-#calls the main function
-main()
-
+print(g.NPOP, g.NSEEDS, g.antennaFile)
 #Reads the veff output file for the antenna, returns the average veff
 def ReadVeff(NSEEDS, antenna_file):
 	veff_list = []
@@ -34,19 +28,28 @@ def ReadVeff(NSEEDS, antenna_file):
 	with open(antenna_file, newline = '') as out_file:
 		out_reader = csv.reader(out_file, delimiter='\t')
 		for output in out_reader:
-			out_list.append(output)
-			
-	for i in range(out_list):
-		veff_list.append(outList[i][1])
-	veff_sum = sum(veffList)
+			out_list.append(str(output))
+	chunks = [[0 for x in range(6)] for y in range(len(out_list))]
+	for i in range(len(out_list)):
+		chunks[i] = re.split(' +', out_list[i])
+	for i in range(len(out_list)):
+		veff_list.append(float(chunks[i][1]))
+	veff_sum = sum(veff_list)
 	veff = veff_sum/NSEEDS
 	return veff
 
 #Writes the fitnessScores data into a csv file
 def WriteFitness(f_scores):
 	with open('fitnessScores.csv', 'w') as csv_file:
-		f.write("The Ohio State University GENETIS Data.\n")
-		f.write("Current generation's fitness scores:\n")
+		csv_file.write("The Ohio State University GENETIS Data.\n")
+		csv_file.write("Current generation's fitness scores:\n")
 		for i in range(g.NPOP):
-			f.write(fitnesScores[i]+"\n")
-
+			csv_file.write(str(f_scores[i])+"\n")
+def main(): #Loops through the antennas,calculates their fitness, and writes them to a csv
+	for i in range(g.NPOP):
+		fitness = ReadVeff(g.NSEEDS, g.antennaFile+"_"+str(i+1)+".txt")
+		fitnessScores.append(fitness)
+	print(fitnessScores)
+	WriteFitness(fitnessScores)
+#calls the main function
+main()

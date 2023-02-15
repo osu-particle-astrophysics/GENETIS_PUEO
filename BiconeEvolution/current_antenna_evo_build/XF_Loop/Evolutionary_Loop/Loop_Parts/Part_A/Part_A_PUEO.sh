d7e1e35c29bc434301e5d35c3ccdf49716377405
Dylan Wells <dylanwells37@gmail.com>
2023-02-15 17:40:06 -0500

    begin adding PUEO as a switch

--- BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Loop_Parts/Part_A/Part_A_PUEO.sh
+++ BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Loop_Parts/Part_A/Part_A_PUEO.sh
@@ -15,11 +15,18 @@ NPOP=$2
 WorkingDir=$3
 RunName=$4
 GeoFactor=$5
+rank_no=$6
+roulette_no=$7
+tournament_no=$8
+reproduction_no=$9
+crossover_no=${10}
+mutation-rate=${11}
+sigma=${12}
 
 cd $WorkingDir
 
 g++ -std=c++11 GA/SourceFiles/New_GA.cpp -o GA/Executables/New_GA.exe
-./GA/Executables/New_GA.exe "PUEO" $gen $NPOP 60 20 20 10 90 10 7
+./GA/Executables/New_GA.exe "PUEO" $gen $NPOP $rank_no $roulette_no $tournament_no $reproduction_no $crossover_no $mutation-rate $sigma
 
 cp generationDNA.csv Run_Outputs/$RunName/${gen}_generationDNA.csv
 mv generators.csv Run_Outputs/$RunName/${gen}_generators.csv

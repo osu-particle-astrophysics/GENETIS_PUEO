e9401fc38e1d0e6f12fbd3a5e2a8240dcd9cacad
Dylan Wells <dylanwells37@gmail.com>
2023-05-16 12:40:42 -0400

    colorblind friendly and errorbars

--- BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/FScorePlotPUEO.py
+++ BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/FScorePlotPUEO.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import argparse
 import csv
 import matplotlib.cm as cm
+import matplotlib as mpl
 import seaborn as sns
 import pandas as pd
  
@@ -26,6 +27,17 @@ fileReadTemp = []
 fScoresGen = []
 fScoresInd = []
 
+
+# let me make an array holding each generation number
+# I'm going to add small random numbers to these so that we can spread apart the data points a bit
+np.random.seed(1) # seed the random number generator so each generation looks the same each time we recreated the plot
+gen_array = []
+for i in range(g.NPOP):
+	gen_num = []
+	for j in range(g.numGens+1):
+		gen_num.append(float(j+np.random.uniform(-2/10,2/10,1)))
+
+	gen_array.append(gen_num)
 
 #Load in the Fitnesses
 
@@ -51,12 +63,25 @@ for ind in range(1, g.NPOP+1):
     #we want to store these now in the arrays containing all the data
     FitnessesArray.append(tempFitnesses)
     tempFitnesses = []
-    
+
+# Load in the Plus and Minus Errors
+PlusErrorArray = []
+MinusErrorArray = []
+for gen in range(0, g.numGens+1):
+    plusErrors, minusErrors = np.loadtxt(g.source + "/{}_errorBars.csv".format(gen), delimiter=',', skiprows=0, unpack=True)
+    PlusErrorArray.append(plusErrors)
+    MinusErrorArray.append(minusErrors)
+#Now transpose the arrays so that they are in the correct format for the violin plot
+PlusErrorArray = np.transpose(PlusErrorArray)
+MinusErrorArray = np.transpose(MinusErrorArray)
+
+
 # Load in the max, min, and max error
-maxFits, minFits, maxErrors = np.loadtxt(g.location + "/plottingData.csv", delimiter=',', skiprows=0, unpack=True)
+maxFits, minFits, maxErrors = np.loadtxt(g.source + "/plottingData.csv", delimiter=',', skiprows=0, unpack=True)
 maxFit = maxFits.max()
 minFit = minFits.min()
 maxError = maxErrors.max()
+diffFit = maxFit - minFit
 
 
 genAxis = np.linspace(0,g.numGens,g.numGens+1,endpoint=True)
@@ -89,25 +114,41 @@ FlippedFitness = np.transpose(FitnessesArray)
 for ind in range(g.numGens+1):	
     mean = sum(FlippedFitness[ind])/g.NPOP
     MeanFitness.append(mean)	
+    FlippedFitness[ind].sort()
     MedianFitness.append(FlippedFitness[ind][int(g.NPOP/2)])
 
+#Setting variables for the plots
+major = 5.0
+minor = 3.0
 
 
+
+mpl.rcParams['text.usetex'] = True
 plt.figure(figsize=(10, 8))
 
+plt.style.use('seaborn')
 #plt.axhline(y=Veff_ARA, linestyle = '--', color = 'k')
 
 colors = cm.rainbow(np.linspace(0, 1, g.NPOP))
-plt.axis([-1, g.numGens+1, minFit - maxError, maxFit + maxError])
 
+# Testing our colorblind friendly colors
+colors2 = ['#00429d', '#3e67ae', '#618fbf', '#85b7ce', '#b1dfdb', '#ffcab9', '#fd9291', '#e75d6f', '#c52a52', '#93003a']
+plt.axis([-1, g.numGens+1, minFit - (diffFit * 0.1) , maxFit + (diffFit * 0.1)])
+plt.plot(genAxis, MeanFitness, linestyle='-', alpha = 1, markersize = 15)
+plt.plot(genAxis, MedianFitness, linestyle='dashed', alpha = 1, markersize = 15)
 
+#plotting with small deviations in the x axis so we can see the data points
 for ind in range(g.NPOP):
     LabelName = "Individual {}".format(ind+1)
-    plt.plot(genAxis, FitnessesArray[ind], label = LabelName, marker = 'o', color = colors[ind], linestyle='', alpha = 0.4, markersize = 11)
-    plt.plot(genAxis, MeanFitness, label = LabelName, linestyle='-', alpha = 1, markersize = 15)
-plt.xlabel('Generation', size = 26)
-plt.ylabel('Fitness Score (km$^3$str)', size = 26)
-plt.title("Fitness Score over Generations (0 - {})".format(int(g.numGens)), size = 30)
+    plt.errorbar(gen_array[ind], FitnessesArray[ind], yerr=[MinusErrorArray[ind], PlusErrorArray[ind]], label = LabelName, marker = 'o', color = colors2[ind%10], linestyle='', alpha = 0.6, markersize = 11, capsize=3, capthick=1)
+
+
+plt.xlabel('Generation', size = 23)
+plt.ylabel('Fitness Score (km$^3$str)', size = 23)
+plt.legend(["Mean", "Median", "Individuals"], loc = 'upper left', prop={'size': 10})
+plt.xticks(genAxis, size = 12)
+plt.yticks(size = 12)
+plt.title("Fitness Score over Generations (0 - {})".format(int(g.numGens)), size = 28)
 
 #plt.legend()
 plt.savefig(g.destination + Plot2DName)
@@ -115,9 +156,12 @@ plt.savefig(g.destination + Plot2DName)
 #create a violin plot of the fitness scores
 #Create a new figure
 plt.figure(figsize=(10, 8))
+plt.style.use('seaborn')
+
+plt.axis([-1, g.numGens+1, minFit - (diffFit * 0.25) , maxFit + (diffFit * 0.25)])
 #Plot the mean and median fitnesses
-plt.plot(genAxis, MeanFitness, linestyle='-', alpha = 1, markersize = 25, color='red')
-plt.plot(genAxis, MedianFitness, linestyle='dashed', alpha = 1, markersize =20, color='green')
+plt.plot(genAxis, MeanFitness, linestyle='-', alpha = 1, markersize = 25, color='#00429d')
+plt.plot(genAxis, MedianFitness, linestyle='dashed', alpha = 1, markersize =20, color='#93003a')
 #We need to create a dataframe in the form of (gen, fitness) for each individual fitness 
 violinArray = []
 for unit in FitnessesArray:
@@ -128,8 +172,10 @@ df0 = pd.DataFrame(violinArray, columns = ['Generation','Fitness Score'])
 #add the violinplot to the figure
 violinplot = sns.violinplot(data=df0, x="Generation", y="Fitness Score", color='lightcyan', width=0.25)
 fig = violinplot.get_figure()
-plt.xlabel('Generation', size = 26)
-plt.ylabel('Fitness Score (km$^3$str)', size = 26)
-plt.title("Fitness Score over Generations (0 - {})".format(int(g.numGens)), size = 30)
+plt.xlabel('Generation', size = 23)
+plt.ylabel('Fitness Score (km$^3$str)', size = 23)
+plt.title("Fitness Score over Generations (0 - {})".format(int(g.numGens)), size = 28)
+plt.xticks(genAxis, size = 12)
+plt.yticks(size = 12)
 plt.legend(["Mean", "Median"])
 fig.savefig(g.destination+ViolinPlotName) 

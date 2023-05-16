5499acb1070f0ed68a06ef71e6db11e663c11a02
Dylan Wells <dylanwells37@gmail.com>
2023-05-16 12:41:31 -0400

    Automatic scaling

--- BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/Rainbow_Plotter_PUEO.py
+++ BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/Rainbow_Plotter_PUEO.py
@@ -10,18 +10,18 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 
-#load in the plotting data
-maxFits, minFits, maxErrors = np.loadtxt(g.location + "/plottingData.csv", delimiter=',', skiprows=0, unpack=True)
-maxFit = maxFits.max()
-minFit = minFits.min()
-maxError = maxErrors.max()
-
 #parser.add_argument("GenNumber", help="Generation number the code is running on (for formatting purposes)", type=int)
 parser.add_argument("location", help="Location of runData.csv and fitness.csv", type=str)
 g = parser.parse_args()
 
+#load in the plotting data
+maxFits, minFits, maxErrors = np.loadtxt(g.location + "/plottingData.csv", delimiter=',', skiprows=0, unpack=True)
+maxFit = maxFits.max()
+minFit = minFits.min()
+
 data = pd.read_csv(g.location+"/testpara.csv")
-fig = px.parallel_coordinates(data, color = "Fitness", color_continuous_scale = px.colors.sequential.Turbo, dimensions = ["SideLength","Height","XInitial","YInital","ZFinal","YFinal","Beta","Generation"],labels={"SideLength": "Side Length (cm)","Height": "Height (cm)","XInitial": "Initial X (cm)","YInital": "Initial Y (cm)","ZFinal": "Final Z (cm)","YFinal": "Final Y (cm)","Beta": "Beta","Generation": "Generation"},color_continuous_midpoint=22500, range_color = [minFit - maxError, maxFit + maxError])
+#We want the range of colors to be from the minimum fitness to the maximum fitness
+fig = px.parallel_coordinates(data, color = "Fitness", color_continuous_scale = px.colors.sequential.Turbo, dimensions = ["SideLength","Height","XInitial","YInital","ZFinal","YFinal","Beta","Generation"],labels={"SideLength": "Side Length (cm)","Height": "Height (cm)","XInitial": "Initial X (cm)","YInital": "Initial Y (cm)","ZFinal": "Final Z (cm)","YFinal": "Final Y (cm)","Beta": "Beta","Generation": "Generation"},color_continuous_midpoint=22500, range_color = [minFit, maxFit])
 fig.write_image(g.location+"/Rainbow_Plot.png")
 
 

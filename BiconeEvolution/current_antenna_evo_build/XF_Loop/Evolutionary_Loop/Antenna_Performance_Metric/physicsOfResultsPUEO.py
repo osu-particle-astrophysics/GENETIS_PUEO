71d90aff109036a8384c7b952944bac677c8f4da
Dylan Wells <dylanwells37@gmail.com>
2023-05-29 16:16:44 -0400

    Add Neutrino and RF dir Plots

--- BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/physicsOfResultsPUEO.py
+++ BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/physicsOfResultsPUEO.py
@@ -36,6 +36,12 @@ ROOT.gSystem.Load("/fs/ess/PAS1960/buildingPueoSim/pueoBuilder/lib/libAnitaEvent
 ROOT.gSystem.Load("/fs/ess/PAS1960/buildingPueoSim/pueoBuilder/lib/libPueoSim.so")
 ROOT.gInterpreter.Declare('#include "Geoid.h"')
 
+#ROOT.gSystem.Load("/users/PAS1960/dylanwells1629/buildingPueoSim/pueoBuilder/lib/libNiceMC.so")
+#ROOT.gSystem.Load("/users/PAS1960/dylanwells1629/buildingPueoSim/pueoBuilder/lib/libAntarcticaRoot.so")
+#ROOT.gSystem.Load("/users/PAS1960/dylanwells1629/buildingPueoSim/pueoBuilder/lib/libAnitaEvent.so")
+#ROOT.gSystem.Load("/users/PAS1960/dylanwells1629/buildingPueoSim/pueoBuilder/lib/libPueoSim.so")
+#ROOT.gInterpreter.Declare('#include "Geoid.h"')
+
 '''
 # Variables used in Dennis' code
 #list of all variable names
@@ -55,13 +61,16 @@ PassingEvents = defaultdict(list)
 PassingWeights = defaultdict(list)
 TotalEvents = defaultdict(list)
 RawWeights = defaultdict(list)
-success_runs = []
 
 var = ['trigg', 'passed', 'passWeight', 'rawWeights', 
        'position', 'neutrinoFlavor', 
        'interactionLength', 'interactionCrossSection',
        'interactionStrength', 'showerPnuEv', 'maxEField', 
-       'maxEFieldFreq', 'nu_e', 'nu_m', 'nu_t']
+       'maxEFieldFreq', 'nu_e', 'nu_m', 'nu_t', 'RFx', 'RFy', 
+       'RFz', 'RFdirCosTheta', 'RFdirTheta', 'nuDirCosTheta', 'nuDirTheta']
+
+waveformV_array = []
+waveformH_array = []
 
 for x in var:
     var_dict['{0}'.format(x)] = []
@@ -75,14 +84,14 @@ def getFiles(source, energy, indiv):
 
     root = source
     print(root)
-    pattern = "IceFinal_*_{}_*.root".format(indiv)
-
+    all_tree_pattern = "IceFinal_allTree_*_{}_*.root".format(indiv)
+    pass_tree_pattern = "IceFinal_passTree_*_{}_*_1.root".format(indiv)
 
     # Walk through the directory and find the root files
     for path, subdirs, files in os.walk(root):
         j = 0
         for name in files:
-            if fnmatch(name, pattern):
+            if fnmatch(name, all_tree_pattern):
                 print(os.path.join(path, name))
                 
                 try:
@@ -90,35 +99,54 @@ def getFiles(source, energy, indiv):
                     fileName = os.path.join(path, name)
                     IceFinalFile = ROOT.TFile(fileName)
                     
-                    nuWeights = []
-                    nuPasses = []
 
                     allTree = IceFinalFile.allTree
                     allEvents = allTree.GetEntries()
-
+                    print(allEvents)
                     TotalEvents[energy].append(allEvents)
-                    passTree = IceFinalFile.passTree
-                    passEvents = passTree.GetEntries()
+                    
                 except:
                     print("Error opening file")
                     continue
-                success_runs.append(fileName)
                 
+            elif fnmatch(name, pass_tree_pattern):
+                print(os.path.join(path, name))
+                
+                try:    
+                    
+                    fileName = os.path.join(path, name)
+                    IceFinalFile = ROOT.TFile(fileName)
+                    
+                    passTree = IceFinalFile.passTree
+                    passEvents = passTree.GetEntries()
+                    
+                    nuWeights = []
+                    nuPasses = []
+                    print(passEvents)
+                    
+                except:
+                    print("Error opening file")
+                    continue
+                
+                
+                waveformsH = []
+                waveformsV = []
                 for i in range(passEvents):
                     j += 1
                     
                     passTree.GetEvent(i)
-                    allTree.GetEvent(i)
-                    
+                
                     # Part from rootAnalysis.py
                     nuPasses.append(1)
                     nuWeights.append(passTree.event.neutrino.path.weight
-                                     /(passTree.event.loop.positionWeight
-                                       *passTree.event.loop.directionWeight))
+                                    /(passTree.event.loop.positionWeight
+                                    *passTree.event.loop.directionWeight))
                     RawWeights[energy].append(nuWeights[-1])
 
                     # collecting varaibles in the dictionary
-
+                    waveformsV = passTree.event.waveformsV
+                    waveformsH = passTree.event.waveformsH
+                    
                     trigg = j
                     var_dict['trigg'].append(j)
                     passed = 1
@@ -134,6 +162,14 @@ def getFiles(source, energy, indiv):
                     showerPnuEv = passTree.event.shower.pnu.eV
                     maxEField = passTree.event.signalSummaryAtDetector.maxEField
                     maxEFieldFreq = passTree.event.signalSummaryAtDetector.maxEFieldFreq
+                    RFdirCosTheta = passTree.event.RFdir.CosTheta()
+                    RFx = passTree.event.RFdir.X()
+                    RFy = passTree.event.RFdir.Y()
+                    RFz = passTree.event.RFdir.Z()
+                    RFdirTheta = passTree.event.RFdir.Theta()
+                    nuDirCosTheta = passTree.event.neutrino.path.direction.CosTheta()
+                    nuDirTheta = passTree.event.neutrino.path.direction.Theta()
+                    
                     if passTree.event.neutrino.flavor == 1:
                         nu_e, nu_m, nu_t = 1, 0, 0
                     elif passTree.event.neutrino.flavor == 2:
@@ -142,24 +178,33 @@ def getFiles(source, energy, indiv):
                         nu_e, nu_m, nu_t = 0, 0, 1
                     
                     all_var = [trigg, passed, passWeight, rawWeights,
-                               position, neutrinoFlavor, 
-                               interactionLength, interactionCrossSection, 
-                               interactionStrength, showerPnuEv, maxEField,
-                               maxEFieldFreq, nu_e, nu_m, nu_t]
+                            position, neutrinoFlavor, 
+                            interactionLength, interactionCrossSection, 
+                            interactionStrength, showerPnuEv, maxEField,
+                            maxEFieldFreq, nu_e, nu_m, nu_t, RFx, RFy, 
+                            RFz, RFdirCosTheta, RFdirTheta, nuDirCosTheta, nuDirTheta]
                     
                     for k, v in enumerate(all_var):
                         var_dict[var[k]].append(v)
                     
                 PassingEvents[energy].append(np.sum(nuPasses))
                 PassingWeights[energy].append(np.sum(nuWeights))
-                
+                waveformH_array.append(waveformsH)
+                waveformV_array.append(waveformsV)
     print("Number of events: {}".format(np.sum(TotalEvents[energy])))
     print("Number of passing events: {}".format(np.sum(PassingEvents[energy])))
     print("Done collecting variables")
 
 getFiles(g.source, g.energy, g.indiv)
 
+print(waveformH_array, waveformV_array)
+print(len(waveformH_array), len(waveformV_array))
+try:
+    print(waveformH_array[0].size(), waveformV_array[0].size())
+except:
+    print("didn't work")
 
+print("Plotting")
 ### Plotting ###
 mpl.rcParams['text.usetex'] = True
 # colorblind friendly colors
@@ -181,36 +226,6 @@ ax1.set_xticklabels(['$\\nu_e$', '$\\nu_\\mu$', '$\\nu_\\tau$'])
 ax1.grid(True)
 fig1.savefig('{}/{}_neutrinoFlavor_bestindiv.png'.format(g.destination, g.indiv))
 
-#plot the interaction length against the interaction cross section
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-ax2.scatter(var_dict['interactionLength'], var_dict['interactionCrossSection'], s=1)
-ax2.set_xlabel('Interaction Length (m)')
-ax2.set_ylabel('Interaction Cross Section (m$^2$)')
-ax2.set_title('Interaction Length vs Interaction Cross Section for {} EeV Neutrinos'.format(g.energy))
-ax2.grid(True)
-fig2.savefig('{}/{}_interactionLength_vs_interactionCrossSection_bestindiv.png'.format(g.destination, g.indiv))
-
-#plot the interaction length against the interaction strength
-fig3 = plt.figure()
-ax3 = fig3.add_subplot(111)
-ax3.scatter(var_dict['interactionLength'], var_dict['interactionStrength'], s=1)
-ax3.set_xlabel('Interaction Length (m)')
-ax3.set_ylabel('Interaction Strength')
-ax3.set_title('Interaction Length vs Interaction Strength for {} EeV Neutrinos'.format(g.energy))
-ax3.grid(True)
-fig3.savefig('{}/{}_interactionLength_vs_interactionStrength_bestindiv.png'.format(g.destination, g.indiv))
-
-#plot the maximum electric field against the maximum electric field frequency
-fig4 = plt.figure()
-ax4 = fig4.add_subplot(111)
-ax4.scatter(var_dict['maxEField'], var_dict['maxEFieldFreq'], s=1)
-ax4.set_xlabel('Maximum Electric Field (V/m)')
-ax4.set_ylabel('Maximum Electric Field Frequency (Hz)')
-ax4.set_title('Maximum Electric Field vs Maximum Electric Field Frequency for {} EeV Neutrinos'.format(g.energy))
-ax4.grid(True)
-fig4.savefig('{}/{}_maxEField_vs_maxEFieldFreq_bestindiv.png'.format(g.destination, g.indiv))
-
 #plot a 2d histogram of the maximum electric field against the maximum electric field frequency
 fig5 = plt.figure()
 ax5 = fig5.add_subplot(111)
@@ -225,3 +240,44 @@ cbar.set_ticks([1, 10, 100, 1000, 10000, 100000])
 cbar.set_ticklabels(['1', '10', '100', '1000', '10000', '100000'])
 fig5.savefig('{}/{}_maxEField_vs_maxEFieldFreq_2d_bestindiv.png'.format(g.destination, g.indiv))
 
+#plot RF direction
+fig6 = plt.figure()
+ax6 = fig6.add_subplot(111)
+ax6.scatter(var_dict['RFx'], var_dict['RFy'], s=1)
+ax6.set_xlabel('RFx')
+ax6.set_ylabel('RFy')
+ax6.set_title('RF Direction for {} EeV Neutrinos'.format(g.energy))
+ax6.grid(True)
+fig6.savefig('{}/{}_RF_direction_bestindiv.png'.format(g.destination, g.indiv))
+
+#Plot a histogram of the RF direction cosine theta
+fig7 = plt.figure()
+ax7 = fig7.add_subplot(111)
+N, bins, patches = ax7.hist(var_dict['RFdirCosTheta'], bins=100, range=(0, 1), align='mid', rwidth=0.8, color=plotting_colors[0])
+ax7.set_xlabel('RF Direction Cosine $\\theta$')
+ax7.set_ylabel('Number of Events')
+ax7.set_title('RF Direction Cosine $\\theta$ for {} EeV Neutrinos'.format(g.energy))
+ax7.grid(True)
+fig7.savefig('{}/{}_RFdirCosTheta_bestindiv.png'.format(g.destination, g.indiv))
+
+#Plot a histogram of the neutrino direction cosine theta
+fig9 = plt.figure()
+ax9 = fig9.add_subplot(111)
+N, bins, patches = ax9.hist(var_dict['nuDirCosTheta'], bins=100, range=(0, 1), align='mid', rwidth=0.8, color = plotting_colors[-1])
+ax9.set_xlabel('Neutrino Direction Cosine $\\theta$')
+ax9.set_ylabel('Number of Events')
+ax9.set_title('Neutrino Direction Cosine $\\theta$ for {} EeV Neutrinos'.format(g.energy))
+ax9.grid(True)
+fig9.savefig('{}/{}_nuDirCosTheta_bestindiv.png'.format(g.destination, g.indiv))
+
+#Plot a histogram of the neutrino direction theta
+fig10 = plt.figure()
+ax10 = fig10.add_subplot(111)
+N, bins, patches = ax10.hist(var_dict['nuDirTheta'], bins=100, range=(0, 3.14), align='mid', rwidth=0.8, color = plotting_colors[-1])
+ax10.set_xlabel('Neutrino Direction $\\theta$ (rad)')
+ax10.set_ylabel('Number of Events')
+ax10.set_title('Neutrino Direction $\\theta$ for {} EeV Neutrinos'.format(g.energy))
+ax10.set_xticks([0, 0.5*np.pi, np.pi])
+ax10.set_xticklabels(['0', '$\\frac{1}{2}\\pi$', '$\\pi$'])
+ax10.grid(True)
+fig10.savefig('{}/{}_nuDirTheta_bestindiv.png'.format(g.destination, g.indiv))

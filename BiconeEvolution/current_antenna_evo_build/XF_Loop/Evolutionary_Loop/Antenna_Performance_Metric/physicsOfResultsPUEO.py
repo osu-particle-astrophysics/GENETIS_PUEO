d9a12d3373d6528f2ac34c8983a18e233124c9b0
Dylan Wells <dylanwells37@gmail.com>
2023-06-01 15:06:58 -0400

    changing formatting of plotting to shorten lines

--- BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/physicsOfResultsPUEO.py
+++ BiconeEvolution/current_antenna_evo_build/XF_Loop/Evolutionary_Loop/Antenna_Performance_Metric/physicsOfResultsPUEO.py
@@ -198,6 +198,7 @@ def getFiles(source, energy, indiv):
                     nuDirTheta = passTree.event.neutrino.path.direction.Theta()
                     
                     
+                    
                     if passTree.event.neutrino.flavor == 1:
                         nu_e, nu_m, nu_t = 1, 0, 0
                     elif passTree.event.neutrino.flavor == 2:
@@ -229,13 +230,15 @@ print("Plotting")
 ### Plotting ###
 mpl.rcParams['text.usetex'] = True
 # colorblind friendly colors
-plotting_colors = ['#00429d', '#4771b2', '#73a2c6', '#a5d5d8', '#ffffe0', '#ffbcaf', '#f4777f', '#cf3759', '#93003a']
+plotting_colors = ['#00429d', '#4771b2', '#73a2c6', '#a5d5d8', '#ffffe0', 
+                   '#ffbcaf', '#f4777f', '#cf3759', '#93003a']
 
 #plotting the number of each type of neutrino that passed
 #Note: 1 = nu_e, 2 = nu_mu, 3 = nu_tau
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(111)
-N, bins, patches = ax1.hist(var_dict['neutrinoFlavor'], bins=3, range=(0.5, 3.5), align='mid', rwidth=0.8)
+N, bins, patches = ax1.hist(var_dict['neutrinoFlavor'], bins=3, 
+                            range=(0.5, 3.5), align='mid', rwidth=0.8)
 patches[0].set_facecolor(plotting_colors[0])
 patches[1].set_facecolor(plotting_colors[4])
 patches[2].set_facecolor(plotting_colors[-1])
@@ -250,7 +253,8 @@ fig1.savefig('{}/{}_neutrinoFlavor_bestindiv.png'.format(g.destination, g.indiv)
 #plot a 2d histogram of the maximum electric field against the maximum electric field frequency
 fig5 = plt.figure()
 ax5 = fig5.add_subplot(111)
-N, xedges, yedges, im = ax5.hist2d(var_dict['maxEField'], var_dict['maxEFieldFreq'], bins=100, range=[[0, max(var_dict['maxEField'])], [0, max(var_dict['maxEFieldFreq'])]], norm=mpl.colors.LogNorm())
+N, xedges, yedges, im = ax5.hist2d(var_dict['maxEField'], var_dict['maxEFieldFreq'], bins=100, 
+                                   range=[[0, max(var_dict['maxEField'])], [0, max(var_dict['maxEFieldFreq'])]], norm=mpl.colors.LogNorm())
 ax5.set_xlabel('Maximum Electric Field (V/m)', fontsize=12)
 ax5.set_ylabel('Maximum Electric Field Frequency (Hz)', fontsize=12)
 ax5.set_title('Maximum Electric Field vs Maximum Electric Field Frequency for {} EeV Neutrinos'.format(g.energy), fontsize=13, fontweight='bold')
@@ -265,7 +269,8 @@ fig5.savefig('{}/{}_maxEField_vs_maxEFieldFreq_2d_bestindiv.png'.format(g.destin
 #Plot a histogram of the RF direction cosine theta
 fig7 = plt.figure()
 ax7 = fig7.add_subplot(111)
-N, bins, patches = ax7.hist(var_dict['RFdirCosTheta'], bins=100, range=(0, 1), align='mid', rwidth=0.8, color=plotting_colors[0])
+N, bins, patches = ax7.hist(var_dict['RFdirCosTheta'], bins=100, 
+                            range=(0, 1), align='mid', rwidth=0.8, color=plotting_colors[0])
 ax7.set_xlabel('RF Direction Cosine $\\theta$')
 ax7.set_ylabel('Number of Events')
 ax7.set_title('RF Direction Cosine $\\theta$ for {} EeV Neutrinos'.format(g.energy))
@@ -275,7 +280,8 @@ fig7.savefig('{}/{}_RFdirCosTheta_bestindiv.png'.format(g.destination, g.indiv))
 #Plot a histogram of the neutrino direction cosine theta
 fig9 = plt.figure()
 ax9 = fig9.add_subplot(111)
-N, bins, patches = ax9.hist(var_dict['nuDirCosTheta'], bins=100, range=(0, 1), align='mid', rwidth=0.8, color = plotting_colors[-1])
+N, bins, patches = ax9.hist(var_dict['nuDirCosTheta'], bins=100, 
+                            range=(0, 1), align='mid', rwidth=0.8, color = plotting_colors[-1])
 ax9.set_xlabel('Neutrino Direction Cosine $\\theta$')
 ax9.set_ylabel('Number of Events')
 ax9.set_title('Neutrino Direction Cosine $\\theta$ for {} EeV Neutrinos'.format(g.energy))
@@ -285,7 +291,8 @@ fig9.savefig('{}/{}_nuDirCosTheta_bestindiv.png'.format(g.destination, g.indiv))
 #Plot a histogram of the neutrino direction theta
 fig10 = plt.figure()
 ax10 = fig10.add_subplot(111)
-N, bins, patches = ax10.hist(var_dict['nuDirTheta'], bins=100, range=(0, 3.14), align='mid', rwidth=0.8, color = plotting_colors[-1])
+N, bins, patches = ax10.hist(var_dict['nuDirTheta'], bins=100, 
+                             range=(0, 3.14), align='mid', rwidth=0.8, color = plotting_colors[-1])
 ax10.set_xlabel('Neutrino Direction $\\theta$ (rad)')
 ax10.set_ylabel('Number of Events')
 ax10.set_title('Neutrino Direction $\\theta$ for {} EeV Neutrinos'.format(g.energy))
